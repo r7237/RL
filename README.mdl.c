d834332ccceadf70daf89cc61a5c1584a7fbdda3
r7237 <35914754+r7237@users.noreply.github.com>
2018-03-02 15:44:18 +0530

    Update README.mdl.c

--- README.mdl.c
+++ README.mdl.c
@@ -1,17 +1,13 @@
-include <stdio.h>
- 
-int binary_conversion(int);
- 
+#include <stdio.h>
+ #include<conio.h>
 int main()
 {
    int num, bin;
- 
    printf("Enter a decimal number: ");
    scanf("%d", &num);
    bin = binary_conversion(num);
    printf("The binary equivalent of %d is %d\n", num, bin);
 }
- 
 int binary_conversion(int num)
 {
     if (num == 0)

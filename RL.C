2e845765bf6f785729487adf28d8cd2ba2f7ca45
r7237 <35914754+r7237@users.noreply.github.com>
2018-03-02 15:46:51 +0530

    Update RL.C

--- RL.C
+++ RL.C
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <string.h>
- 
+#INCLUDE<CONIO.H> 
 void main()
 {
     char s[200];
     int count = 0, i;
- 
     printf("enter the string\n");
     scanf("%[^\n]s", s);
     for (i = 0;s[i] != '\0';i++)
